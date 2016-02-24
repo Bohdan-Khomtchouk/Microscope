a0787bbc833f5303a8ef0127096cf333170ed47d
Bohdan-Khomtchouk <bohdan@stanford.edu>
2016-02-24 18:05:35 -0500

    GO and network analysis

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 ## About
 
-MicroScope is an RShiny and JavaScript (D3.js) software program designed to produce dynamic, interactive heatmaps in a web browser. MicroScope allows you to magnify any portion of a heatmap by a simple click-and-drag feature to zoom in, and a click-once feature to zoom out. MicroScope is designed with large heatmaps in mind (e.g., gene expression heatmaps with thousands of genes), where individual entries quickly become unreadable as more and more add up. However, MicroScope allows you to repeatedly zoom in to any sector of the heatmap to investigate a region, cluster, or even a single gene. You can scroll up and down the page of your web browser to see more genes than fit your window. MicroScope also allows you to hover the mouse pointer over any specific gene to show precise expression level details.  In addition to visual magnification, MicroScope also allows users to analytically perform real-time statistical analyses with simple button clicks.  As such, MicroScope presents a significant advance in heatmap visualization technology over current standard protocols. 
+MicroScope is an R Shiny and JavaScript (D3.js) software program designed to produce dynamic, interactive heatmaps in an Internet browser.  MicroScope allows you to magnify any portion of a heatmap by a simple click-and-drag feature to zoom in, and click-once feature to zoom out.  MicroScope is designed with large heatmaps in mind (e.g., gene expression heatmaps with thousands of genes), where individual entries quickly become unreadable as more are added.  However, MicroScope allows you to repeatedly zoom in to any sector of the heatmap to investigate a region, cluster, or even a single gene.  MicroScope also allows you to hover the mouse pointer over any specific gene to show precise expression level details.  In addition to visual magnification, MicroScope also allows users to analytically perform real-time statistical analyses, as well as perform gene ontology and network analyses, with simple button clicks.
 
-MicroScope is an ongoing bioinformatics software project fully financially supported by the United States Department of Defense (DoD) through the National Defense Science and Engineering Graduate Fellowship (NDSEG) Program. This research was conducted with Government support under and awarded by DoD, Army Research Office (ARO), National Defense Science and Engineering Graduate (NDSEG) Fellowship, 32 CFR 168a.
+MicroScope is an ongoing bioinformatics software project financially supported by the United States Department of Defense (DoD) through the National Defense Science and Engineering Graduate Fellowship (NDSEG) Program. This research was conducted with Government support under and awarded by DoD, Army Research Office (ARO), National Defense Science and Engineering Graduate (NDSEG) Fellowship, 32 CFR 168a.
 
-Please cite: "Khomtchouk BB, Dargis-Robinson V, Hennessy JR, Wahlestedt C.  “MicroScope: real-time statistical analytics platform and visualization engine for gene expression heatmaps” (submitted). bioRxiv doi: http://dx.doi.org/10.1101/034694" within any source that makes use of any methods inspired by MicroScope. 
+Please cite: "Khomtchouk BB, Dargis-Robinson V, Hennessy JR, Wahlestedt C.  “MicroScope: comprehensive genome analysis software suite for gene expression heatmaps” (submitted). bioRxiv doi: http://dx.doi.org/10.1101/034694" within any source that makes use of any methods inspired by MicroScope. 
 
 ## Usage (for general public)
 
@@ -31,15 +31,27 @@ Please cite: "Khomtchouk BB, Dargis-Robinson V, Hennessy JR, Wahlestedt C.  “M
 * `library(RColorBrewer)`
 * `install.packages("htmlwidgets")`
 * `library(htmlwidgets)`
+* `install.packages("networkD3")`
+* `library(networkD3)`
+* `install.packages("data.table")`
+* `library(data.table)`
+* `install.packages("dplyr")`
+* `library(dplyr)`
 * `source("https://bioconductor.org/biocLite.R")`
 * `biocLite("edgeR")`
 * `library(edgeR)`
+* `biocLite("GO.db")`
+* `library(GO.db)`
+* `biocLite("goseq")`
+* `library(goseq)`
 * `runApp("microscope")`
 
 ## Screenshots
 
-<img width="1440" alt="picc1" src="https://cloud.githubusercontent.com/assets/9893806/12105623/74e1c930-b324-11e5-965e-08d523617f49.png">
+<img width="1440" alt="heat" src="https://cloud.githubusercontent.com/assets/9893806/13304097/e36c1558-db20-11e5-8b79-83d69fd56ef0.png">
 
-<img width="1440" alt="picc2" src="https://cloud.githubusercontent.com/assets/9893806/12105624/771091be-b324-11e5-8676-e121ab2d84f7.png">
+<img width="1440" alt="stats" src="https://cloud.githubusercontent.com/assets/9893806/13304104/ea4a1320-db20-11e5-9dbc-f4028f16c6c7.png">
 
-<img width="1440" alt="picc3" src="https://cloud.githubusercontent.com/assets/9893806/12105625/78933474-b324-11e5-8294-c6306f243f3b.png">
+<img width="1440" alt="go" src="https://cloud.githubusercontent.com/assets/9893806/13304107/eccd508a-db20-11e5-99c2-3ed8f24dd44a.png">
+
+<img width="1440" alt="network" src="https://cloud.githubusercontent.com/assets/9893806/13304111/f0eb4d70-db20-11e5-83b1-b57c3f9a4985.png">
--- microscope/server.r
+++ microscope/server.r
@@ -2,20 +2,20 @@
 
 # This file is part of MicroScope.
 
-# MicroScope is an RShiny and JavaScript (D3.js) software program designed to produce dynamic, interactive heatmaps in an Internet browser.
+# MicroScope is an R Shiny and JavaScript (D3.js) software program designed to produce dynamic, interactive heatmaps in an Internet browser.
 # MicroScope allows you to magnify any portion of a heatmap by a simple click-and-drag feature to zoom in, and click-once feature to zoom out.
 # MicroScope is designed with large heatmaps in mind (e.g., gene expression heatmaps with thousands of genes), where individual entries quickly become unreadable as more are added. 
 # However, MicroScope allows you to repeatedly zoom in to any sector of the heatmap to investigate a region, cluster, or even a single gene.  
 # MicroScope also allows you to hover the mouse pointer over any specific gene to show precise expression level details.
-# In addition to visual magnification, MicroScope also allows users to analytically perform real-time statistical analyses with simple button clicks.
+# In addition to visual magnification, MicroScope also allows users to analytically perform real-time statistical analyses, as well as perform gene ontology and network analyses, with simple button clicks.
 
-# For more information, please see: "Khomtchouk BB, Dargis-Robinson V, Hennessy JR, Wahlestedt C. “MicroScope: real-time statistical analytics and visualization platform for gene expression heatmaps”. bioRxiv doi: http://dx.doi.org/10.1101/034694"
+# For more information, please see: "Khomtchouk BB, Dargis-Robinson V, Hennessy JR, Wahlestedt C. "MicroScope: real-time statistical analytics and visualization platform for gene expression heatmaps". bioRxiv doi: http://dx.doi.org/10.1101/034694"
 
-# MicroScope is an ongoing bioinformatics software project fully financially supported by the United States Department of Defense (DoD) 
+# MicroScope is an ongoing bioinformatics software project financially supported by the United States Department of Defense (DoD) 
 # through the National Defense Science and Engineering Graduate Fellowship (NDSEG) Program. This research was conducted with Government support 
 # under and awarded by DoD, Army Research Office (ARO), National Defense Science and Engineering Graduate (NDSEG) Fellowship, 32 CFR 168a.
 
-# Please cite: "Khomtchouk BB, Dargis-Robinson V, Hennessy JR, Wahlestedt C. “MicroScope: real-time statistical analytics and visualization platform for gene expression heatmaps”. bioRxiv doi: http://dx.doi.org/10.1101/034694" within any source that makes use of any methods inspired by MicroScope.
+# Please cite: "Khomtchouk BB, Dargis-Robinson V, Hennessy JR, Wahlestedt C. "MicroScope: real-time statistical analytics and visualization platform for gene expression heatmaps". bioRxiv doi: http://dx.doi.org/10.1101/034694" within any source that makes use of any methods inspired by MicroScope.
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -37,104 +37,274 @@ library(d3heatmap)
 library(htmlwidgets)
 library(tools)
 library(edgeR)
+library(GO.db)
+library(goseq)
+library(networkD3)
+library(data.table)
+library(dplyr)
 
 
 # backend 
 server <- shinyServer(function(input, output) {	
-
-    #instructions tab
-    output$text1 <- renderText({ "0) You can easily make a .csv file by simply saving your Microsoft Excel workbook as a .csv through 'Save As'.  Before saving as a .csv, your Excel file should look something like:" })
-    output$text2 <- renderText({ "Please note that all cell values must be positive (i.e., corresponding to raw gene expression values: read counts per gene per sample).  After you upload your .csv file, you can log-transform your data to normalize it (and the resultant heatmap) automatically within MicroScope." })
-    output$text3 <- renderText({ "1) After uploading a .csv file, navigate to the Heatmap panel to see the resultant heatmap.  Click and drag anywhere in the heatmap to zoom in.  Click once to zoom out." })
-    output$text4 <- renderText({ "2) To perform statistical analysis on your heatmap, specify your control samples in the sidebar panel marked 'Specify Control Samples:' (by default, the remaining samples will be designated as experimental samples).  After pressing the Do Stats! button, your statistical table will appear in the Statistical Analysis panel. Positive values of log(FC) (i.e., fold change) indicate upregulation in experimental samples (i.e., experimental samples have higher expression values relative to controls). Negative values indicate downregulation in experimental samples (i.e., control samples have higher expression values relative to experimentals). " })
-    output$text5 <- renderText({ "3) Feel free to download either the heatmap or the statistical analysis table to your computer using the buttons provided.  For more information, please visit the MicroScope publication." })
-
-
-	#file upload
-	datasetInput <- reactive({
-    	inFile <- input$filename
-    	if (is.null(inFile)) return(NULL)
-    	read.table(inFile$datapath, header= TRUE, sep=",", quote='"', row.names=1)
-							})	
-							
-	
-	#edgeR prep
-	output$ctrlcolumns <- renderUI({
-    	df <- datasetInput()
-    	if (is.null(df)) return(NULL)
-    	ctrlcolumns <- names(df)
-    	selectInput('ctrlcolumns', 'Specify Control Samples:', choices = ctrlcolumns, multiple = TRUE)
-    })			
-	
-	
-	#edgeR statistical engine
-	stats <- reactive({
-		group <- as.numeric(names(datasetInput()) %in% input$ctrlcolumns)
-		y <- DGEList(counts = datasetInput(), group = group)
-		y <- calcNormFactors(y)
-		y <- estimateCommonDisp(y)
-		y <- estimateTagwiseDisp(y)
-		et <- exactTest(y)
-		results <- topTags(et, n=50000)
-		results_df <- as.data.frame(results)
-	})
-	
-	
-	#statistical analysis table
-	output$table <- renderTable({
-		if (input$goButton == 0) {return()}        
-    	else{
-        	stats()
-        	}
-		}, digits = -3)
-	
-	
-	#statistical table download
-	output$downloadtable <- downloadHandler(
-    	filename = function() {
-    		paste(basename(file_path_sans_ext(input$filename)), '_edgeR_stats_table', '.csv', sep='')
-    	},
-    	content = function(file) {
-    		if (input$goButton != 0) write.csv(stats(), file) else return()
-    		}
-    	)
-    	
-
-	#log2 data transformation
-	log2_datasetInput <- reactive({
-		cpm(datasetInput(), prior.count=2, log=TRUE)
-	})
-	
-	
-	#d3heatmap prep					
-	plot <- reactive({
- 		d3heatmap( 
- 			if (input$log2_transformed_data) log2_datasetInput() else datasetInput(),
-      		cexRow = as.numeric(as.character(input$xfontsize)),
-      		cexCol = as.numeric(as.character(input$yfontsize)),
-      		colors = input$choose,
-      		k_row = input$color_row_branches,
-      		k_col = input$color_column_branches,
-      		dendrogram = input$dendrogram
-    			 )  
-    })
-    
-	
-	#d3heatmap output
-	output$heatmap <- renderD3heatmap({
-		if(!is.null(datasetInput()))
-        	plot()
-    })
-    
-  	
-  	#d3heatmap download								
-    output$downloadHeatmap <- downloadHandler(
-        filename = function() {
-            paste0(basename(file_path_sans_ext(input$filename)), '.html')
-        },
-        content = function(file) {
-            saveWidget(plot(), file)
-        }
+  
+  # instructions tab
+  output$text1 <- renderText({ "0) You can easily make a .csv file by simply saving your Microsoft Excel workbook as a .csv through 'Save As'.  Before saving as a .csv, your Excel file should look something like:" })
+  output$text2 <- renderText({ "Please note that all cell values must be positive (i.e., corresponding to raw gene expression values, i.e., read counts per gene per sample).  After you upload your .csv file, you can log-transform your data to normalize it (and the resultant heatmap) automatically within MicroScope." })
+  output$text3 <- renderText({ "1) After uploading a .csv file, navigate to the Heatmap panel to see the resultant heatmap.  Click and drag anywhere in the heatmap to zoom in.  Click once to zoom out." })
+  output$text4 <- renderText({ "2) To perform statistical analysis on your heatmap, specify your control samples in the sidebar panel marked 'Specify Control Samples:' (by default, the remaining samples will be designated as experimental samples).  After pressing the 'Run Statistics' button, your statistical table will appear in the Statistical Analysis panel. Positive values of log(FC) (i.e., fold change) indicate upregulation in experimental samples (i.e., experimental samples have higher expression values relative to controls). Negative values indicate downregulation in experimental samples (i.e., control samples have higher expression values relative to experimentals). " })
+  output$text5 <- renderText({ "3) Feel free to download either the heatmap or the statistical analysis table to your computer using the buttons provided." })  
+  output$text6 <- renderText({ "4) After generating a heatmap and running statistics on it, navigate to the Gene Ontology panel for information about performing GO analysis on the top differentially expressed genes in your heatmap.  You may then download the gene ontology results to your computer." })  
+  output$text7 <- renderText({ "5) After performing GO analysis, navigate to the Network Analysis panel for information about performing network analysis on the top differentially expressed genes in your heatmap.  You may interact with the resultant network by clicking, zooming, and dragging any element of the network.  You may also download the network analysis results to your computer." })  
+  output$text8 <- renderText({ "6) For more information about this software, please visit the MicroScope publication." })
+  
+  
+  # file upload
+  datasetInput <- reactive({
+    validate(
+    	need(input$filename != 0, "To generate a heatmap, please select a file for input") 
     )
+    inFile <- input$filename
+    if (is.null(inFile)) return(NULL)
+    read.table(inFile$datapath, header= TRUE, sep=",", quote='"', row.names=1)
+  })	
+  
+  
+  # log2 data transformation
+  log2_datasetInput <- reactive({
+    cpm(datasetInput(), prior.count=2, log=TRUE)
+  })
+
+
+  # d3heatmap prep					
+  plot <- reactive({
+    d3heatmap( 
+      if (input$log2_transformed_data) log2_datasetInput() else datasetInput(),
+      cexRow = as.numeric(as.character(input$xfontsize)),
+      cexCol = as.numeric(as.character(input$yfontsize)),
+      colors = input$choose,
+      k_row = input$color_row_branches,
+      k_col = input$color_column_branches,
+      dendrogram = input$dendrogram
+    )  
+  })
+  
+  
+  # d3heatmap output
+  output$heatmap <- renderD3heatmap({
+    if(!is.null(datasetInput()))
+      plot()
+  })
+  
+  
+  # d3heatmap download								
+  output$downloadHeatmap <- downloadHandler(
+    filename = function() {
+      paste0(basename(file_path_sans_ext(input$filename)), '.html')
+    },
+    content = function(file) {
+      saveWidget(plot(), file)
+    }
+  )
+  
+  
+  # edgeR prep
+  output$ctrlcolumns <- renderUI({
+    df <- datasetInput()
+    if (is.null(df)) return(NULL)
+    ctrlcolumns <- names(df)
+    selectInput('ctrlcolumns', 'Specify Control Samples:', choices = ctrlcolumns, multiple = TRUE)
+  })			
+  
+  
+  # edgeR statistical engine
+  stats <- reactive({
+    group <- as.numeric(names(datasetInput()) %in% input$ctrlcolumns)
+    y <- DGEList(counts = datasetInput(), group = group)
+    y <- calcNormFactors(y)
+    y <- estimateCommonDisp(y)
+    y <- estimateTagwiseDisp(y)
+    et <- exactTest(y)
+    results <- topTags(et, n=50000)
+    results_df <- as.data.frame(results)
+  })
+
+
+  # statistical analysis table
+  `%then%` <- shiny:::`%OR%`
+  output$table <- renderTable({
+    if (input$goButton == 0) {return(validate(
+    	need(input$filename != 0, "To run statistics, please first select a file for input") %then%
+        need(input$goButton != 0, "To run statistics, please select your control samples under 'Specify Control Samples' then click 'Run Statistics'")
+    ))}        
+    else {
+      stats()
+    }
+  }, digits = -3)
+  
+  
+  # statistical table download
+  output$downloadtable <- downloadHandler(
+    filename = function() {
+      paste(basename(file_path_sans_ext(input$filename)), '_edgeR_stats_table', '.csv', sep='')
+    },
+    content = function(file) {
+      if (input$goButton != 0) write.csv(stats(), file) else return()
+    }
+  )
+
+   
+  # create enriched gene database from GO analysis
+  enriched <- reactive({
+
+    group <<- as.numeric(names(datasetInput()) %in% input$ctrlcolumns)
+    y <<- DGEList(counts = datasetInput(), group = group)
+    y <<- calcNormFactors(y)
+    y <<- estimateCommonDisp(y)
+    y <<- estimateTagwiseDisp(y)
+    et <<- exactTest(y)
     
+    genes <<- as.integer(p.adjust(et$table$PValue[et$table$logFC!=0], method="BH") < input$cutoffP)
+    genesP <<- as.integer(p.adjust(et$table$PValue, method="BH") < input$cutoffP)
+    names(genes) = row.names(et$table[et$table$logFC!=0,])
+    pwf <<- nullp(genes, input$Genome, input$geneRef)
+    pwf_1 <<- pwf[which(pwf[,1] == 1),]
+    rownamespwfOne <<- row.names(pwf_1)
+    
+	callData <<- reactive({ 
+      if(input$Genome == "mm9"){
+        library("org.Mm.eg.db")
+      }
+      else if(input$Genome == "hg38"){
+        library("org.Hs.eg.db")
+      }
+    }) 
+  
+    GO.wall <<- goseq(pwf, input$Genome, input$geneRef)
+    
+    if(input$chooseEnriched == "FDR") ({
+    	FDR.enriched.GO <<- GO.wall$category[p.adjust(GO.wall$over_represented_pvalue, method="BH") < input$cutoffFdr]
+    })
+    else if(input$chooseEnriched == "P-value") ({
+    	enriched.GO <<- GO.wall$category[(GO.wall$over_represented_pvalue) < input$cutoffP]
+    })
+    
+  })
+  
+  
+  # output the enriched genes
+  output$gene_ontology <- renderPrint({
+  
+    if (input$goData == 0) {return(validate(
+      need(input$filename != 0, "For gene ontology analysis, please first select a file for input") %then%
+      need(input$goButton != 0, "For gene ontology analysis, please first select your control samples under 'Specify Control Samples' then click 'Run Statistics'") %then%
+      need(input$goData != 0, "For gene ontology analysis, please select a genome under 'Choose Genome Database', choose the appropriate gene identifier, choose how many top gene ontologies to display, choose how to stratify your top gene ontologies (e.g., by p-value or by FDR), choose the respective statistical cutoff, and then click 'Do Gene Ontology Analysis'.  Now wait while your genome-wide annotations are automatically downloaded from UCSC (this may take, on average, between 15 seconds and 2 minutes, depending on your network connection).  Your gene ontology results will appear shortly.")
+    ))}        
+    else {
+      enriched()
+      if(input$chooseEnriched == "FDR") ({
+        enriched.GO = FDR.enriched.GO   
+      })
+    }
+    
+  if(!length(enriched.GO) == 0) {
+    for(go in enriched.GO[1:input$numberGenes]) {
+      print(GOTERM[[go]])
+      cat("--------------------------------------\n")
+    }
+  }
+  else{
+    print("No enriched genes present at this statistical cutoff level")
+  }
+  
+  })
+  
+  
+  # simple network engine prep
+  grow <- function(nominal_GO_df, rownamespwfOne, enriched.GO, Genome, topEnriched) ({
+    total = list()
+    getGo <- getgo(rownamespwfOne, Genome, input$geneRef)
+    Base = data.table(rownamespwfOne, getGo)
+    for(go in nominal_GO_df$ind[1:topEnriched]){
+        Names <- Base$rownamespwfOne[grep(go,Base$getGo)]
+        nameList = list(Names) 
+        total = append(total, nameList)
+    }
+    names(total) <- nominal_GO_df$ind[1:topEnriched]
+    total <- total
+  })
+  
+  
+  # simple network engine
+  network <- reactive({
+    
+    if(input$chooseEnriched == "FDR")({
+    	enriched.GO <- GO.wall$category[p.adjust(GO.wall$over_represented_pvalue, method="BH") < input$cutoffFdr]
+    })
+    else if(input$chooseEnriched == "P-value")({
+    	enriched.GO <- GO.wall$category[(GO.wall$over_represented_pvalue) < input$cutoffP]
+    })
+    
+    
+    #network analysis loop (FDR vs nominal p-value) within simple network engine
+    if(!length(enriched.GO) == 0){
+    	nominal_GO_df <- stack(lapply(mget(enriched.GO, GOTERM, ifnotfound = NA), Term)) 
+    
+    	total <- grow(nominal_GO_df, rownamespwfOne, input$chooseEnriched, input$Genome, input$numberGenes)
+    	namesGo <- names(total)
+    	vector = c()
+    	targetVector = c()
+    
+    	for (go in 1:length(namesGo)){
+    		lengthName <- length(total[[go]])
+    		for(populate in 1:lengthName){
+    			nameGene <- nominal_GO_df$values[grep(namesGo[go], nominal_GO_df$ind)]
+      			vector = append(vector, nameGene)
+      			targetVector = append(targetVector, total[[go]][populate])
+      		}
+    	}
+    
+    	src <- vector
+    	target <- targetVector
+    	networkData <- data.frame(src, target)   
+    	simpleNetwork(networkData, charge = -1000, opacity = 1, zoom = T)
+    }
+    
+	else {
+		return(validate(need(input$goData == 0, "No enriched genes are available to make a network at this statistical cutoff level")))
+    }
+    
+  })
+  
+  
+  # simple network output
+  output$networkData <- renderSimpleNetwork({
+	if (input$doNets == 0) {return(validate(
+      need(input$filename != 0, "For network analysis, please first select a file for input") %then%
+      need(input$goButton != 0, "For network analysis, please first select your control samples under 'Specify Control Samples' then click 'Run Statistics'") %then%
+      need(input$goData != 0, "For network analysis, please first select a genome under 'Choose Genome Database' then click 'Do Gene Ontology Analysis'") %then%
+      need(input$doNets != 0, "For network analysis, please click on 'Do Network Analysis' and wait for about 5-30 seconds")
+    ))}
+	else {
+      network()
+    }
+  })
+  
+  
+  # simple network download								
+  output$downloadSimpleNetwork <- downloadHandler(
+    filename = function() {
+      paste0(basename(file_path_sans_ext(input$filename)), '_network', '.html', sep='')
+    },
+    content = function(file) {
+    	if (input$doNets != 0) {
+    		saveWidget(network(), file)
+    	} 
+    	else {
+    		return()
+    	}
+    }
+  )
+  
+
+  
 })
