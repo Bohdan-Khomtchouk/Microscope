cfc57e7df080f9ab017add3522d34a1e453a071e
Bohdan Khomtchouk <bohdankhomtchouk@UM-31HFG8WM.local>
2016-05-23 22:07:20 -0400

    add PCA feature

--- microscope/server.r
+++ microscope/server.r
@@ -1,34 +1,6 @@
-# Copyright (C) 2015-2016 Bohdan Khomtchouk, Vytas Dargis-Robinson, and James Hennessy
+# Copyright (C) 2015-2016 Bohdan Khomtchouk, James Hennessy, and Vytas Dargis-Robinson
 
 # This file is part of MicroScope.
-
-# MicroScope is an R Shiny and JavaScript (D3.js) software program designed to produce dynamic, interactive heatmaps in an Internet browser.
-# MicroScope allows you to magnify any portion of a heatmap by a simple click-and-drag feature to zoom in, and click-once feature to zoom out.
-# MicroScope is designed with large heatmaps in mind (e.g., gene expression heatmaps with thousands of genes), where individual entries quickly become unreadable as more are added. 
-# However, MicroScope allows you to repeatedly zoom in to any sector of the heatmap to investigate a region, cluster, or even a single gene.  
-# MicroScope also allows you to hover the mouse pointer over any specific gene to show precise expression level details.
-# In addition to visual magnification, MicroScope also allows users to analytically perform real-time statistical analyses, as well as perform gene ontology and network analyses, with simple button clicks.
-
-# For more information, please see: "Khomtchouk BB, Dargis-Robinson V, Hennessy JR, Wahlestedt C. "MicroScope: real-time statistical analytics and visualization platform for gene expression heatmaps". bioRxiv doi: http://dx.doi.org/10.1101/034694"
-
-# MicroScope is an ongoing bioinformatics software project financially supported by the United States Department of Defense (DoD) 
-# through the National Defense Science and Engineering Graduate Fellowship (NDSEG) Program. This research was conducted with Government support 
-# under and awarded by DoD, Army Research Office (ARO), National Defense Science and Engineering Graduate (NDSEG) Fellowship, 32 CFR 168a.
-
-# Please cite: "Khomtchouk BB, Dargis-Robinson V, Hennessy JR, Wahlestedt C. "MicroScope: real-time statistical analytics and visualization platform for gene expression heatmaps". bioRxiv doi: http://dx.doi.org/10.1101/034694" within any source that makes use of any methods inspired by MicroScope.
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # ------------------------------------------------------------------------------------
 
@@ -42,6 +14,7 @@ library(goseq)
 library(networkD3)
 library(data.table)
 library(dplyr)
+library(png)
 
 
 # backend 
@@ -114,6 +87,10 @@ server <- shinyServer(function(input, output) {
   })
   
   
+  # Unlock features (PCA and stats UI) after heatmap upload
+  outputOptions(output, 'heatmap', suspendWhenHidden = FALSE)
+  
+  
   # d3heatmap download								
   output$downloadHeatmap <- downloadHandler(
     filename = function() {
@@ -123,6 +100,98 @@ server <- shinyServer(function(input, output) {
       saveWidget(plot(), file)
     }
   )
+  
+  
+  # PCA biplot
+  output$pca_biplot <- renderPlot({
+    validate(
+    	need(input$pcaButton !=  0, "To generate a biplot, follow the instructions above, then click 'Run PCA'") 
+    	)
+    if (length(PCA) != 0){
+    	df <- datasetInput()
+    
+  		rownames(df) <- c()
+    
+  		dm <- data.matrix(df)
+        
+  		if (input$Type == "Covariance Matrix") {
+      
+  			PCA <<- prcomp(dm, scale = TRUE)
+    
+  		}  
+    
+  		else if (input$Type == "Correlation Matrix") {
+      
+    
+  			PCA <<- prcomp(dm, scale = FALSE)
+    
+  		}
+
+		biplot(PCA, scale = 0)
+		mtext("Biplot", line = 3, col = "black", font = 2, cex = 1.2)
+    } 
+  })
+  
+  
+  # PCA screeplot 
+  output$pca_screeplot <- renderPlot({
+    validate(
+    	need(input$pcaButton != 0, "To generate a screeplot, follow the instructions above, then click 'Run PCA'") 
+    	)
+    if(length(PCA) != 0){
+    	df <- datasetInput()
+    
+  		rownames(df) <- c()
+    
+  		dm <- data.matrix(df)
+        
+  		if (input$Type == "Covariance Matrix") {
+      
+  			PCA <<- prcomp(dm, scale = TRUE)
+    
+  		}  
+    
+  		else if (input$Type == "Correlation Matrix") {
+      
+    
+  			PCA <<- prcomp(dm, scale = FALSE)
+    
+  		}
+
+    	screeplot(Screeplot, type = "lines")
+    }
+  })
+  
+  
+  # PCA summary info
+  output$pca_summary_table <- renderPrint({
+    if (input$pcaButton == 0) {return(validate(
+      need(input$filename != 0, "Table of PCA summary: To conduct principal components analysis, please first select a file for input") %then%
+        need(input$goButton != 0, "Table of PCA summary: To conduct principal components analysis, click 'Run PCA'")
+    ))}        
+    else {
+    	df <- datasetInput()
+    
+  		rownames(df) <- c()
+    
+  		dm <- data.matrix(df)
+        
+  		if (input$Type == "Covariance Matrix") {
+      
+  			PCA <<- prcomp(dm, scale = TRUE)
+    
+  		}  
+    
+  		else if (input$Type == "Correlation Matrix") {
+      
+    
+  			PCA <<- prcomp(dm, scale = FALSE)
+    
+  		}
+
+    	summary(PCA)
+    }
+  }) 
   
   
   # edgeR prep
--- microscope/ui.r
+++ microscope/ui.r
@@ -1,36 +1,9 @@
-# Copyright (C) 2015-2016 Bohdan Khomtchouk, Vytas Dargis-Robinson, and James Hennessy
+# Copyright (C) 2015-2016 Bohdan Khomtchouk, James Hennessy, and Vytas Dargis-Robinson
 
 # This file is part of MicroScope.
 
-# MicroScope is an R Shiny and JavaScript (D3.js) software program designed to produce dynamic, interactive heatmaps in an Internet browser.
-# MicroScope allows you to magnify any portion of a heatmap by a simple click-and-drag feature to zoom in, and click-once feature to zoom out.
-# MicroScope is designed with large heatmaps in mind (e.g., gene expression heatmaps with thousands of genes), where individual entries quickly become unreadable as more are added. 
-# However, MicroScope allows you to repeatedly zoom in to any sector of the heatmap to investigate a region, cluster, or even a single gene.  
-# MicroScope also allows you to hover the mouse pointer over any specific gene to show precise expression level details.
-# In addition to visual magnification, MicroScope also allows users to analytically perform real-time statistical analyses, as well as perform gene ontology and network analyses, with simple button clicks.
-
-# For more information, please see: "Khomtchouk BB, Dargis-Robinson V, Hennessy JR, Wahlestedt C. "MicroScope: real-time statistical analytics and visualization platform for gene expression heatmaps". bioRxiv doi: http://dx.doi.org/10.1101/034694"
-
-# MicroScope is an ongoing bioinformatics software project fully financially supported by the United States Department of Defense (DoD) 
-# through the National Defense Science and Engineering Graduate Fellowship (NDSEG) Program. This research was conducted with Government support 
-# under and awarded by DoD, Army Research Office (ARO), National Defense Science and Engineering Graduate (NDSEG) Fellowship, 32 CFR 168a.
-
-# Please cite: "Khomtchouk BB, Dargis-Robinson V, Hennessy JR, Wahlestedt C. "MicroScope: real-time statistical analytics and visualization platform for gene expression heatmaps". bioRxiv doi: http://dx.doi.org/10.1101/034694" within any source that makes use of any methods inspired by MicroScope.
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,c
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 # ------------------------------------------------------------------------------------
+
 library(shiny)
 library(d3heatmap)
 library(RColorBrewer)
@@ -57,10 +30,14 @@ ui <- shinyUI(pageWithSidebar(
     sliderInput("xfontsize", "Choose X Font Size:", min = 0.3, max = 2, value = 0.5),
     sliderInput("yfontsize", "Choose Y Font Size:", min = 0.3, max = 2, value = 1.0),
     downloadButton("downloadHeatmap", "Download Heatmap"),
-    uiOutput("ctrlcolumns"),
-    actionButton("goButton", "Run Statistics"),
-    downloadButton("downloadtable", "Download Stats Table")
-    ,
+    conditionalPanel(
+      condition = "output.heatmap",
+      selectInput("Type", "Choose PCA Option:", selected = "Correlation Matrix", c("Covariance Matrix", "Correlation Matrix")),
+      actionButton("pcaButton", "Run PCA"),
+      uiOutput("ctrlcolumns"),
+      actionButton("goButton", "Run Statistics"), 
+      downloadButton("downloadtable", "Download Stats Table")
+    ),
     conditionalPanel(
       condition = "input.goButton == true",
       selectInput("Genome", "Choose Genome Database:", selected = "mm9", c("Mouse" = "mm9", "Human" = "hg38", "Chimpanzee" = "panTro4", "Rat" = "rn6", "Worm" = "ce10", "Zebrafish" = "danRey10", "Fly" = "dm6", "Yeast" = "sacCer3", "Frog" = "xenTro7", "Chicken" = "galGal4", "Pig" = "susScr3", "Cow" = "bosTau8", "Dog" = "canFam3")),
@@ -69,7 +46,7 @@ ui <- shinyUI(pageWithSidebar(
       selectInput("chooseEnriched", "Stratify Top Gene Ontologies By:", c("P-value", "FDR")),
       sliderInput("cutoffP", "Choose Gene Ontology P-value Cutoff:", min = 0.001, max = 0.1, value = 0.05),
 	  sliderInput("cutoffFdr", "Choose Gene Ontology FDR Cutoff:", min = 0.001, max = 0.1, value = 0.05),
-	  actionButton("goData","Do Gene Ontology Analysis")
+	  actionButton("goData", "Do Gene Ontology Analysis")
     ),
     conditionalPanel(
       condition = "input.goData == true",
@@ -83,6 +60,7 @@ ui <- shinyUI(pageWithSidebar(
     tabsetPanel(
       tabPanel("Instructions", textOutput("text1"), img(src='excel.png'), textOutput("text2"), textOutput("text3"), textOutput("text4"), textOutput("text5"), textOutput("text6"), textOutput("text7"), textOutput("text8")),
       tabPanel("Heatmap", uiOutput("heatmapOutput")),
+      tabPanel("PCA", verbatimTextOutput("pca_summary_table"), plotOutput("pca_screeplot"), plotOutput("pca_biplot")),
       tabPanel("Statistical Analysis", tableOutput("table")),
       tabPanel("Gene Ontology" , verbatimTextOutput("gene_ontology")),
 	  tabPanel("Network Analysis", simpleNetworkOutput("networkData", width= "100%", height="1500px"))
